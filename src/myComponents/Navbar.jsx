df65cabcd4b9c0365ba55b8f52486d1dd8e2dfe6
Musfiqur-Rahman0 <musfiqurrhaman6@gmail.com>
2025-05-10 16:53:57 +0600

    updated the navbar

--- src/myComponents/Navbar.jsx
+++ src/myComponents/Navbar.jsx
@@ -14,9 +14,9 @@ const Navbar = () => {
           <h2 className="text-3xl font-bold ">Monster</h2>
           <div className="flex items-center  gap-5 text-sm">
             <Link to="/">Home</Link>
+            <Link to="/projects">Projects</Link>
             <Link to="/about">About</Link>
             <Link to="/contact">Contact</Link>
-            <Link to="/projects">Projects</Link>
             <ModeToggle />
           </div>
         </nav>
